f2dd17252652bd50183974da093fffdea101d0bc
Thomas <mustierthomas@gmail.com>
2025-03-13 11:15:13 +0100

    Initial Commit

---
+++ Medication/MedicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicationApp.swift
+//  Medication
+//
+//  Created by Thomas Mustier on 13/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MedicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MedicationTests/MedicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  MedicationTests.swift
+//  MedicationTests
+//
+//  Created by Thomas Mustier on 13/03/2025.
+//
+
+import Testing
+@testable import Medication
+
+struct MedicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
